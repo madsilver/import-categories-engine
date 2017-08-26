6ee580a7948f98cb80663dcaebaa3528f9befde9
Rodrigo Prata <rodrigo-prata@hotmail.com>
2017-08-25 23:42:57 -0300

    Update test

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -1,15 +1,34 @@
 <?php
 
 class BuilderTest extends \PHPUnit\Framework\TestCase
-{
-    public function testUrlCategoryWithSlug()
+{   
+    public function testSlugCategory()
     {
         $builder = new \Silver\Builder('store', 1);
-        $payload = $builder->payload(array(
+        $category = array(
             'name' => 'cátẽgöry@nâmè',
             'parent_id' => ''
-        ));
+        );
+        $payload = $builder->payload($category);
 
         $this->assertEquals($payload['url_key'], 'category-name');
+    }
+    
+    public function testShouldReturnIdOfCategory()
+    {
+        $builder = new \Silver\Builder('store', 1);
+        $response = array(
+            'id' => 1000,
+            'slug' => 'category-parent'
+        );
+        $builder->setCategories($response);
+
+        $category = array(
+            'name' => 'category-name',
+            'parent_id' => 'category-parent'
+        );
+        $payload = $builder->payload($category);
+        
+        $this->assertEquals($payload['parent_id'], 1000);
     }
 }
